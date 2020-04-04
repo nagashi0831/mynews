14d203d3a5c0caf9bf93438fd50e19394fd3f4c2
”長島壮汰” <gmc_sou.custam@icloud.com>
2020-04-04 15:17:29 +0000

    マイグレーションからのバリデーション

--- app/Http/Controllers/Admin/Newscontroller.php
+++ app/Http/Controllers/Admin/Newscontroller.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+    //以下を追記することでNews modelが使えるようになる
+    use App\News;
+
 class Newscontroller extends Controller
 {
     //以下を追記
@@ -15,6 +18,27 @@ class Newscontroller extends Controller
     
     public function create(Request $request)
     {
+        //varidationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        /*フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパス
+        を保存する*/
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = null;
+        }
+        //フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        //フォームから送られてきたimageを削除する
+        unset($form['image']);
+
+        //データベースに保存する
+        $news->fill($form);
+        $news->save();
+
         return redirect('admin/news/create');
     }
 }
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,12 +5,26 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add(){
         return view('admin.profile.create');
     }
-    public function create(){
+    
+    public function create(Request $request){
+        //varidationを行う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
     public function edit(){
@@ -18,5 +32,6 @@ class ProfileController extends Controller
     }
     public function update(){
         return redirect('admin/profile/edit');
+        
     }
 }
