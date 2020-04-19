f48e5c1d0a1e502aa372bc1ecba9897c89a8d995
”長島壮汰” <gmc_sou.custam@icloud.com>
2020-04-19 11:21:27 +0000

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (\App::environment('production')){
-            \URL::forceschema('https');
+            \URL::forceSchema('https');
         }
     }
 }
